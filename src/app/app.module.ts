d4606f0dc34bd77fa1778c99baa6c369bbacd3c6
ranarahulfz <ranarahulfz@gmail.com>
2019-05-30 20:18:42 +0530

    commit to update application toolbar and add footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutComponent } from './pages/about/about.component';
-import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { AppFooterComponent } from './components/app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    AppToolbarComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
